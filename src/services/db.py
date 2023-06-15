cb88db748be9d7cbea00086f0b56923779874610
hector ramon leon <payorayo@hectors-MacBook-Pro.local>
2023-06-14 20:04:29 -0700

    Feat: save users

--- src/services/db.py
+++ src/services/db.py
@@ -1,4 +1,5 @@
 from src.data.model.category import CategoryModel
+from src.data.model.user import UserModel
 
 
 def get_categories():
@@ -12,3 +13,12 @@ def get_categories():
         _categories = []
     _categories = [_key.to_dict() for _key in _categories]
     return _categories
+
+
+def get_all_user_by_category_id(category_id: str):
+    """
+    Get all user by category id
+    :param category_id:
+    :return:
+    """
+    return list(UserModel.scan(filter_condition=UserModel.categories.contains(category_id)))
