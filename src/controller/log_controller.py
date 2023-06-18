1430beaf95409477523670ce7e04cf18a788e194
hector ramon leon <payorayo@hectors-MacBook-Pro.local>
2023-06-17 18:39:12 -0700

    Fix: fixed bug

--- src/controller/log_controller.py
+++ src/controller/log_controller.py
@@ -10,26 +10,19 @@ class LogController:
         self.logger = _logger
         self.event = _event
 
+
     def get_log_list(self):
         response = []
         logs = get_logs()
-        users = {}
-        categories = {}
 
         for item in logs:
-            if item.user_id not in users:
-                users[item.user_id] = get_user(item.user_id)
-
-            if item.category_id not in categories:
-                categories[item.category_id] = get_category(item.category_id)
-
             response.append({
                 "log_id": item.log_id,
                 "user_id": item.user_id,
-                "user_name": users[item.user_id].name,
-                "email": users[item.user_id].email,
-                "phone_number": users[item.user_id].phone_number,
-                "category": categories[item.category_id].name,
+                "user_name": item.user_name,
+                "email": item.email,
+                "phone_number": item.phone_number,
+                "category": item.category_name,
                 "channel": item.notification_type,
                 "message": item.message,
                 "create_at": item.createdAt.strftime('%Y-%m-%d %H:%M')
--- tests/controller/test_log_controller.py
+++ tests/controller/test_log_controller.py
@@ -17,10 +17,14 @@ class TestUserModel:
 
 
 class TestLogModel:
-    def __init__(self, log_id, user_id, category_id, notification_type, message, createdAt):
+    def __init__(self, log_id, user_id, user_name, email, phone_number, category_id, category_name, notification_type, message, createdAt):
         self.log_id = log_id
         self.user_id = user_id
+        self.user_name = user_name
+        self.email = email
+        self.phone_number = phone_number
         self.category_id = category_id
+        self.category_name = category_name
         self.notification_type = notification_type
         self.message = message
         self.createdAt = createdAt
@@ -57,18 +61,12 @@ class TestLogController:
         }
 
         logs_data = [
-            TestLogModel("l001", "u001", "c001", "movie", "hello", datetime.datetime(2000, 1, 1, 0, 0)),
-            TestLogModel("l002", "u001", "c001", "finance", "again", datetime.datetime(2000, 1, 1, 0, 0)),
+            TestLogModel("l001", "u001", "one", "u001@test.co", "111", "c001", "category one", "movie", "hello", datetime.datetime(2000, 1, 1, 0, 0)),
+            TestLogModel("l002", "u001", "one", "u001@test.co", "111", "c001", "category one", "finance", "again", datetime.datetime(2000, 1, 1, 0, 0)),
         ]
 
         mocker.patch.object(log_controller, 'get_logs')
         log_controller.get_logs.return_value = logs_data
-
-        mocker.patch.object(log_controller, 'get_category')
-        log_controller.get_category.return_value = TestCategoryModel("c001", "category one")
-
-        mocker.patch.object(log_controller, 'get_user')
-        log_controller.get_user.return_value = TestUserModel("u001", "one", "u001@test.co", "111")
 
         log = log_controller.LogController({}, None)
         response = log.get_log_list()
